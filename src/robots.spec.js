3e18346c407654957367a6f7b996c265552f041f
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-11-01 11:37:53 +0000

    test data

--- src/robots.spec.js
+++ src/robots.spec.js
@@ -2,7 +2,10 @@ const test = require('tape');
 const robots = require('./robots.js');
 
 test('robots', function (t) {
-  t.plan(1);
+  t.plan(3);
 
-  t.equal(typeof robots, 'function');
+  const bounds = robots('5 3');
+  t.equal(bounds('1 1 E')('RFRFRFRF'), '1 1 E');
+  t.equal(bounds('3 2 N')('FRRFLLFFRRFLL'), '3 3 N LOST');
+  t.equal(bounds('0 3 W')('LLFFFLFLFL'), '2 3 S');
 });

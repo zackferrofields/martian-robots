ede44f6a439c9cf1c0a0fcbb448889930050b9a3
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-11-07 21:12:33 +0000

    stdin

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-process.stdout.write(`Hello World!\n`);
+const robot = require('./src/robot.js');
+const bounds = robot.process('5 3');
+let input = {scents: []};
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    const[position, instructions] = chunk.split('|');
+    input.position = position;
+    input = bounds(instructions)(input);
+    process.stdout.write(`${input.position}\n`);
+  }
+});

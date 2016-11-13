d9f57dc954228ba65fcbd8e80b08777147cf6d74
Zack Ferro-Fields <zackferrofields@gmail.com>
2016-11-13 12:42:51 +0000

    process args

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # martian-robots
+
+```
+  npm start -- '5 3'  // => node index.js 5 3
+  1 1 E | RFRFRFRF // => 1 1 E
+  3 2 N | FRRFLLFFRRFLL // => 3 3 N LOST
+  0 3 W | LLFFFLFLFL // => 2 3 S
+```
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const robot = require('./src/robot.js');
-const bounds = robot.process('5 3');
+const bounds = robot.process(process.argv[2] || '5 3');
 let input = {scents: []};
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
-  var chunk = process.stdin.read();
+  const chunk = process.stdin.read();
   if (chunk !== null) {
     const[position, instructions] = chunk.split('|');
     input.position = position;
